9aa723e73d2d58afd72fc24902b780a298bd56e7
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-05-18 22:54:42 +0200

    my first commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,8 @@ const app = express()
 app.use(express.json()) // json'lari okuyabilmesi icin
 app.use(express.urlencoded({ extended: false }))
 
-app.use('/api/goals', require('./routes/goalRoutes')) // burda route'lari belirtiyorum
+app.use('/api/goals', require('./routes/goalRoutes')) // goals icin db'ye yapilacak isteklerin urlsini burda belirtiyorum
+app.use('/api/users', require('./routes/userRoutes')) // users icin db'ye yapilacak isteklerin urlsini burda belirtiyorum
 
 app.use(errorHandler)
 
