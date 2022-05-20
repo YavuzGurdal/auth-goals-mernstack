3e84640a4997287e7e95431d63d26da5a11e9b80
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-05-20 22:43:50 +0200

    Frontend Register finish

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,10 @@
 import { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
+import { register, reset } from '../features/auth/authSlice'
+import Spinner from '../components/Spinner'
 
 function Register() {
     const [formData, setFormData] = useState({
@@ -11,16 +16,50 @@ function Register() {
 
     const { name, email, password, password2 } = formData // state'i destructuring yaptik
 
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    const { user, isError, isSuccess, isLoading, message } = useSelector((state) => state.auth)
+
+    useEffect(() => {
+        if (isError) {
+            toast.error(message)
+        }
+
+        if (isSuccess || user) {
+            navigate('/')
+        }
+
+        dispatch(reset())
+
+    }, [user, isError, isSuccess, message, navigate, dispatch])
+
     const onChange = (e) => {
         setFormData((prevState) => ({ // bu sekilde bir defa yazarak hallediyoruz
             ...prevState,
-            [e.target.name]: e.target.value
+            [e.target.name]: e.target.value,
             // key'i e.target.name den aliyoruz. value'yu e.target.value'dan aliyoruz
         }))
     }
 
     const onSubmit = (e) => {
         e.preventDefault()
+
+        if (password !== password2) {
+            toast.error('Passwords do not match')
+        } else {
+            const userData = {
+                name,
+                email,
+                password,
+            }
+
+            dispatch(register(userData))
+        }
+    }
+
+    if (isLoading) {
+        return <Spinner />
     }
 
     return (
