a2a75d4c288c648e3e5f87e9b0845b7372eb53e3
Yavuz <yavuz@Yavuz-MacBook-Pro.local>
2022-05-22 19:47:38 +0200

    upload settings

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
+// siteyi yayinlamak icin
+const path = require('path')
 const express = require('express')
 const colors = require('colors')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
+const { sendFile } = require('express/lib/response')
 const port = process.env.PORT
 
 connectDB() // fonksiyonu burda calistiriyorum
@@ -14,6 +17,18 @@ app.use(express.urlencoded({ extended: false }))
 
 app.use('/api/goals', require('./routes/goalRoutes')) // goals icin db'ye yapilacak isteklerin urlsini burda belirtiyorum
 app.use('/api/users', require('./routes/userRoutes')) // users icin db'ye yapilacak isteklerin urlsini burda belirtiyorum
+
+// siteyi yayinlamak icin
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+    ))
+} else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+}
 
 app.use(errorHandler)
 
